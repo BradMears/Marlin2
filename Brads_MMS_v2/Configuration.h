5f6c89a72afabd84725f15efd5c31951a289b0ad
Brad Mears <brad.mears@gmail.com>
2022-09-10 13:11:43 -0600

    Correct last (?) error from using wrong config file

--- Brads_MMS_v2/Configuration.h
+++ Brads_MMS_v2/Configuration.h
@@ -548,7 +548,7 @@
  *   999 : Dummy Table that ALWAYS reads 100°C or the temperature defined below.
  *
  */
-#define TEMP_SENSOR_0 99
+#define TEMP_SENSOR_0 1
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
@@ -556,7 +556,7 @@
 #define TEMP_SENSOR_5 0
 #define TEMP_SENSOR_6 0
 #define TEMP_SENSOR_7 0
-#define TEMP_SENSOR_BED 99
+#define TEMP_SENSOR_BED 1
 #define TEMP_SENSOR_PROBE 0
 #define TEMP_SENSOR_CHAMBER 0
 #define TEMP_SENSOR_COOLER 0
